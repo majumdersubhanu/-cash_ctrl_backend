6d8d82899caea925c47d68a2b21264de1eb1f8fb
Subhanu Majumder <reach.subhanu@gmail.com>
2024-04-13 23:32:16 +0530

    Description field added in transaction model

--- transactions/models.py
+++ transactions/models.py
@@ -20,10 +20,11 @@ class Transaction(models.Model):
     transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPE_CHOICES)
     category = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.name} ({self.transaction_type})"
 
     class Meta:
-        ordering = ['-date']
+        verbose_name_plural = 'Transactions'
--- transactions/serializers.py
+++ transactions/serializers.py
@@ -5,5 +5,5 @@ from .models import Transaction
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
-        fields = ['id', 'name', 'payment_type', 'transaction_type', 'amount', 'date', 'category']
+        fields = ['id', 'name', 'payment_type', 'transaction_type', 'amount', 'description', 'date', 'category']
         read_only_fields = ['id', 'date']  # 'date' is auto-added
